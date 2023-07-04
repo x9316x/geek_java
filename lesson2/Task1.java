ea60f00ad6a4c03882be5a43ca4873c9843ff6b6
x9316x <x9316x@gmail.com>
2023-07-04 14:36:46 +0300

    add tasks 1, 2 from lesson 2

--- lesson2/Task1.java
+++ lesson2/Task1.java
@@ -1,7 +1,36 @@
 /**
- * Task1
+ * Дана строка sql-запроса "select * from students where ".
+ * Сформируйте часть WHERE этого запроса, используя 
+ * StringBuilder. Данные для фильтрации приведены ниже 
+ * в виде json-строки.
  */
-public class Task1 {
+package lesson2;
 
-    
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Task1 {
+    public static void main(String[] args) {
+        String json = "{\"name\":\"Ivanov\", \"country\":\"Russia\", \"city\":\"Moscow\", \"age\":\"null\"}";
+        StringBuilder sql = new StringBuilder("select * from students where ");
+
+        Pattern pattern = Pattern.compile("\"(.*?)\":\"(.*?)\"");
+        Matcher matcher = pattern.matcher(json);
+
+        while (matcher.find()) {
+            String key = matcher.group(1);
+            String value = matcher.group(2);
+
+            if (!value.equals("null")) {
+                sql.append(key).append(" = '").append(value).append("' AND ");
+            }
+        }
+
+        
+        if (sql.toString().endsWith("AND ")) {
+            sql.setLength(sql.length() - 4);
+        }
+
+        System.out.println(sql.toString());
+    }
 }
--- lesson2/Task2.java
+++ lesson2/Task2.java
@@ -1,3 +1,35 @@
+/**
+ * Реализуйте алгоритм сортировки пузырьком числового массива,
+ * результат после каждой итерации запишите в лог-файл.
+ */
+package lesson2;
+
+import java.io.PrintWriter;
+import java.io.IOException;
+
 public class Task2 {
-    
+    void bubbleSort(int arr[], String fileName) {
+        int n = arr.length;
+        try (PrintWriter writer = new PrintWriter(fileName, "UTF-8")) {
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+                writer.println(java.util.Arrays.toString(arr));
+            }
+        } catch (IOException e) {
+            System.out.println("Произошла ошибка: " + e.getMessage());
+        }
+    }
+
+    public static void main(String args[]) {
+        Task2 bs = new Task2();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        bs.bubbleSort(arr, "log.txt");
+    }
 }
